032aa0e85a9a37078e44fdad6e31cf1946c464a3
7wanaio <1296032384@qq.com>
2024-06-05 17:03:28 +0800

    feat(nextjs-base): created new object

---
+++ apps/nextjs-base/next.config.js
@@ -0,0 +1,20 @@
+import { fileURLToPath } from "url";
+
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: [
+   
+    "@acme/ui"
+
+  ],
+
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default config;
